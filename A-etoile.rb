fe442ad630d50a3e56cdb80c170dc6e369dc7f89
U-CORP\gvelay <gvelay@LFR020145.corp.capgemini.com>
2018-12-05 14:17:30 +0100

    initGraph

--- A-etoile.rb
+++ A-etoile.rb
@@ -1,108 +1,103 @@
-PositiveInfinity = +1.0/0.0
+PosInf=+1.0/0.0
+tSpaceShip={}
+tSurface=[]
 
-def main
-  ptVaisseau={}
-  pts=[]
-  
-  def formatPt(pt)
-    ptFmt=pt.clone
-    ptFmt[:x]=(ptFmt[:x]*596)/7000
-    ptFmt[:y]=(ptFmt[:y]*255)/3000
-    return ptFmt
-  end
-  
-  def formatPts(pts)
-    ptsFmt = pts.clone
-    ptsFmt.each do |pt|
-      pt[:x]=(pt[:x]*596)/7000
-      pt[:y]=(pt[:y]*255)/3000
-    end
-    return ptsFmt
-  end
 
-  if ARGV[0].to_i==0
-    ptVaisseau={x:6500,y:2600}
-    
-    pts << {x:0,y:450}
-    pts << {x:300,y:750}
-    pts << {x:1000,y:450}
-    pts << {x:1500,y:650}
-    pts << {x:1800,y:850}
-    pts << {x:2000,y:1950}
-    pts << {x:2200,y:1850}
-    pts << {x:2400,y:2000}
-    pts << {x:3100,y:1800}
-    pts << {x:3150,y:1550}
-    pts << {x:2500,y:1600}
-    pts << {x:2200,y:1550}
-    pts << {x:2100,y:750}
-    pts << {x:2200,y:150}
-    pts << {x:3200,y:150}
-    pts << {x:3500,y:450}
-    pts << {x:4000,y:950}
-    pts << {x:4500,y:1450}
-    pts << {x:5000,y:1550}
-    pts << {x:5500,y:1500}
-    pts << {x:6000,y:950}
-    pts << {x:6999,y:1750}
-  else
-    ptVaisseau={x:6500,y:2000}
-
-    pts << {x:0,y:1800}
-    pts << {x:300,y:1200}
-    pts << {x:1000,y:1550}
-    pts << {x:2000,y:1200}
-    pts << {x:2500,y:1650}
-    pts << {x:3700,y:220}
-    pts << {x:4700,y:220}
-    pts << {x:4750,y:1000}
-    pts << {x:4700,y:1650}
-    pts << {x:4000,y:1700}
-    pts << {x:3700,y:1600}
-    pts << {x:3750,y:1900}
-    pts << {x:4000,y:2100}
-    pts << {x:4900,y:2050}
-    pts << {x:5100,y:1000}
-    pts << {x:5500,y:500}
-    pts << {x:6200,y:800}
-    pts << {x:6999,y:600}
-  end
-
-  open('A-etoile.ps', 'w') { |f|
-    f.puts "%!PS-Adobe-3.0"
-    f.puts "%%BoundingBox: 0 0 596 792"
-    f.puts "1 0 0 setrgbcolor"
-    f.puts "3 setlinewidth"
-    ptsFmt=formatPts(pts)
-    ptsFmt.each_cons(2) do |pt1, pt2|
-      f.puts "#{pt1[:x]} #{pt1[:y]} moveto"
-      f.puts "#{pt2[:x]} #{pt2[:y]} lineto"
-      f.puts "stroke"
-    end
-    ptVaisFmt = formatPt(ptVaisseau)
-    f.puts "#{ptVaisFmt[:x]} #{ptVaisFmt[:y]} 3 0 360 arc"
-    f.puts "fill"
-    f.puts "stroke"  
-  }
-  system('ps2pdf A-etoile.ps')
-  
-=begin
-     ptVaisFmt = formatPt(ptVaisseau)
-  i=0
-  50.times{
-    open('A-etoile.ps', 'a') { |f|
-      f.puts "#{ptVaisFmt[:x]-i} #{ptVaisFmt[:y]} 3 0 360 arc"
-      f.puts "fill"
-      f.puts "stroke"
-    }
-    system('ps2pdf A-etoile.ps')
-    i+=5
-    sleep 0.1
-  }
-=end
+def formatPt(pt)
+  ptFmt=pt.clone
+  ptFmt[:x]=(ptFmt[:x]*596)/7000
+  ptFmt[:y]=(ptFmt[:y]*255)/3000
+  return ptFmt
 end
 
-main
+def formatSurface(tSurface)
+  tSurfaceFmt = tSurface.clone
+  tSurfaceFmt.each do |pt|
+    pt[:x]=(pt[:x]*596)/7000
+    pt[:y]=(pt[:y]*255)/3000
+  end
+  return tSurfaceFmt
+end
+
+if ARGV[0].to_i==0
+  tSpaceShip={x:6500,y:2600}
+  
+  tSurface << {x:0,y:450}
+  tSurface << {x:300,y:750}
+  tSurface << {x:1000,y:450}
+  tSurface << {x:1500,y:650}
+  tSurface << {x:1800,y:850}
+  tSurface << {x:2000,y:1950}
+  tSurface << {x:2200,y:1850}
+  tSurface << {x:2400,y:2000}
+  tSurface << {x:3100,y:1800}
+  tSurface << {x:3150,y:1550}
+  tSurface << {x:2500,y:1600}
+  tSurface << {x:2200,y:1550}
+  tSurface << {x:2100,y:750}
+  tSurface << {x:2200,y:150}
+  tSurface << {x:3200,y:150}
+  tSurface << {x:3500,y:450}
+  tSurface << {x:4000,y:950}
+  tSurface << {x:4500,y:1450}
+  tSurface << {x:5000,y:1550}
+  tSurface << {x:5500,y:1500}
+  tSurface << {x:6000,y:950}
+  tSurface << {x:6999,y:1750}
+else
+  tSpaceShip={x:6500,y:2000}
+
+  tSurface << {x:0,y:1800}
+  tSurface << {x:300,y:1200}
+  tSurface << {x:1000,y:1550}
+  tSurface << {x:2000,y:1200}
+  tSurface << {x:2500,y:1650}
+  tSurface << {x:3700,y:220}
+  tSurface << {x:4700,y:220}
+  tSurface << {x:4750,y:1000}
+  tSurface << {x:4700,y:1650}
+  tSurface << {x:4000,y:1700}
+  tSurface << {x:3700,y:1600}
+  tSurface << {x:3750,y:1900}
+  tSurface << {x:4000,y:2100}
+  tSurface << {x:4900,y:2050}
+  tSurface << {x:5100,y:1000}
+  tSurface << {x:5500,y:500}
+  tSurface << {x:6200,y:800}
+  tSurface << {x:6999,y:600}
+end
+=begin
+open('A-etoile.ps', 'w') { |f|
+  f.puts "%!PS-Adobe-3.0"
+  f.puts "%%BoundingBox: 0 0 596 792"
+  f.puts "1 0 0 setrgbcolor"
+  f.puts "3 setlinewidth"
+  tSurfaceFmt=formatSurface(tSurface)
+  tSurfaceFmt.each_cons(2) do |pt1, pt2|
+    f.puts "#{pt1[:x]} #{pt1[:y]} moveto"
+    f.puts "#{pt2[:x]} #{pt2[:y]} lineto"
+    f.puts "stroke"
+  end
+  ptVaisFmt = formatPt(tSpaceShip)
+  f.puts "#{ptVaisFmt[:x]} #{ptVaisFmt[:y]} 3 0 360 arc"
+  f.puts "fill"
+  f.puts "stroke"  
+}
+system('ps2pdf A-etoile.ps')
+
+tSpaceShipFmt = formatPt(tSpaceShip)
+i=0
+50.times{
+  open('A-etoile.ps', 'a') { |f|
+    f.puts "#{tSpaceShipFmt[:x]-i} #{tSpaceShipFmt[:y]} 3 0 360 arc"
+    f.puts "fill"
+    f.puts "stroke"
+  }
+  system('ps2pdf A-etoile.ps')
+  i+=5
+  sleep 0.1
+}
+=end
 
 def _findWithCostInf(tE,e)
   return tE.find{|e0|e0[:x]==e[:x]&&e0[:y]==e[:y]&&e0[:cost]<e[:cost]}!=nil
@@ -157,47 +152,81 @@ class NaivePriorityQueue
   end
 end
 
-def segment(tPt1, tPt2)
-  tSegment=[]
+def segment2Array(tPt1, tPt2)
+  tSeg2Ar=[]
   tPtI=[]
   tPtF=[]
-  if tPt2[:x]>tPt1[:x] || (tPt2[:x]==tPt1[:x] && tPt2[:y]>tPt1[:y])
-    tPtI=tPt1.clone
-    tPtF=tPt2.clone
-  else
-    tPtI=tPt2.clone
-    tPtF=tPt1.clone
-  end
-  if tPtI[:x]!=tPtF[:x]
+  if tPt2[:x]!=tPt1[:x]
+    if tPt2[:x]>tPt1[:x]
+      tPtI=tPt1.clone
+      tPtF=tPt2.clone
+    else
+      tPtI=tPt2.clone
+      tPtF=tPt1.clone
+    end
     nDX=tPtF[:x]-tPtI[:x]
     nDY=tPtF[:y]-tPtI[:y]
     nCoef=nDY.to_f/nDX.to_f
     for i in tPtI[:x]..tPtF[:x] do
-      tSegment<< {x:i,y:tPtI[:y]+((i-tPtI[:x])*nCoef).round}
-    end
-  else
-    for i in tPtI[:y]..tPtF[:y] do
-      tSegment<< {x:tPtI[:x],y:i}
+      tSeg2Ar<< {x:i,y:tPtI[:y]+((i-tPtI[:x])*nCoef).round}
     end
   end
-  return tSegment
+  if tPt2[:y]!=tPt1[:y]
+    if tPt2[:y]>tPt1[:y]
+      tPtI=tPt1.clone
+      tPtF=tPt2.clone
+    else
+      tPtI=tPt2.clone
+      tPtF=tPt1.clone
+    end
+    nDX=tPtF[:x]-tPtI[:x]
+    nDY=tPtF[:y]-tPtI[:y]
+    nCoef=nDX.to_f/nDY.to_f
+    for i in tPtI[:y]..tPtF[:y] do
+      tSeg2Ar<< {x:tPtI[:x]+((i-tPtI[:y])*nCoef).round,y:i}
+    end
+  end
+  tSeg2Ar.uniq!
+  return tSeg2Ar
 end
 
 def initGraph(tSurface, tBord)
   tGraph=[]
-  tSurface.each do |pt|
-
+  for i in 1..6999 do
+    for j in 1..2999 do
+      #a continuer
+    end
   end
+  tSurfaceCpy=tSurface.clone
+  tSurfaceCpy.each do |pt|
+    pt[:cost]=PosInf
+    pt[:heuristic]=PosInf
+  end
+  tGraph+=tSurfaceCpy
+  tBordCpy=tBord.clone
+  tBord.each do |pt|
+    pt[:cost]=PosInf
+    pt[:heuristic]=PosInf
+  end
+  tGraph+=tBord
   return tGraph
 end
 
 def border
   bord=[]
-  bord+=segment({x:0,y:0},{x:596,y:0})
-  bord+=segment({x:596,y:0},{x:596,y:255})
-  bord+=segment({x:596,y:255},{x:0,y:255})
-  bord+=segment({x:0,y:255},{x:0,y:0})
+  bord+=segment2Array({x:0,y:0},{x:7000,y:0})
+  bord+=segment2Array({x:7000,y:0},{x:7000,y:3000})
+  bord+=segment2Array({x:7000,y:3000},{x:0,y:3000})
+  bord+=segment2Array({x:0,y:3000},{x:0,y:0})
   return bord
+end
+
+def obstacle(tSurface)
+  tObstacle=[]
+  tSurface.each_cons(2) do |pt1, pt2|
+    tObstacle+=segment2Array(pt1,pt2)
+  end
+  return tObstacle
 end
 
 q = NaivePriorityQueue.new
@@ -215,8 +244,8 @@ p neighbors({x:3,y:7})
 tFather={}
 tFather[{x:7,y:13}]={x:0,y:0}
 tFather[{x:3,y:7}]={x:7,y:13}
-p reconstructPath(tFather, {x:3,y:7})
-
-p segment({x:-5,y:3},{x:-2,y:-3})
+#p reconstructPath(tFather, {x:3,y:7})
+#p segment2Array({x:-2,y:-3},{x:2,y:3})
 #p border
-p segment({x:0,y:0},{x:1,y:255})
+#p initGraph(obstacle(tSurface),border)
+p obstacle(tSurface)
